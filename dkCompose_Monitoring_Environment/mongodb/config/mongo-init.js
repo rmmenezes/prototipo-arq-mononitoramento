6e1f8fafdaea7c089b8e1a6d4518006ce267b16b
rmmenezes <ra29fa@gmail.com>
2020-09-17 02:05:15 -0300

    merge old pc

--- dkCompose_Monitoring_Environment/mongodb/config/mongo-init.js
+++ dkCompose_Monitoring_Environment/mongodb/config/mongo-init.js
@@ -2,17 +2,8 @@
 db.auth('root', 'root')
 
 
-// Indica o nome da base de dados a ser usado pelo usuario a ser criado
-db = db.getSiblingDB('database')
-
-// Cria o usuario e indicando as credenciais e premissões de acesso na base de dados indicada
-db.createUser({
-  user: 'user-mongo',
-  pwd: 'password123',
-  roles: [
-    {
-      role: 'root',
-      db: 'admin',
-    },
-  ],
-});
+> db.createUser({
+  user: 'ian',
+  pwd: 'secretPassword',
+  roles: [{ role: 'readWrite', db:'cool_db'}]
+})
